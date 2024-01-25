614c8fe079a558217a66d6ef2d0e9b87d416afe2
Rafael Silva <rafa3lalv3s19@gmail.com>
2024-01-25 16:54:11 +0000

    dev: add and remove some rules from  the commitlint file

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
-		'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'dev', 'batata']],
+		'type-enum': [2, 'always', ['docs', 'fix', 'refactor', 'revert', 'test', 'dev', 'merge']],
 	},
 }
