81c5339794b9c3ed7b9336370068cc578c59b22e
Nuno <nunojllemos@gmail.com>
2024-02-08 17:36:38 +0100

    fix: delete package-lock.json

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,23 @@
+'use client'
 import React from 'react'
+import { Input } from '@components/ui/input'
+import { Label } from '@components/ui/label'
 
 const Home = () => {
 	return (
-		<div className="p-8">
+		<div className="p-8 flex gap-12 flex-col items-center mx-auto max-w-96">
 			<h1 className="text-5xl">Login</h1>
+
+			<form method="POST" onSubmit={(e) => e.preventDefault()} className="w-full" action="">
+				<div>
+					<Label htmlFor="email">Email</Label>
+					<Input id="email" />
+				</div>
+				<div className="mt-6">
+					<Label htmlFor="password">Password</Label>
+					<Input id="password" />
+				</div>
+			</form>
 		</div>
 	)
 }
