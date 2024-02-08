53019482744530ee9750cd35ab6ad6ca8e312965
Nuno <nunojllemos@gmail.com>
2024-02-08 22:26:03 +0100

    test: testing login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,35 @@
 'use client'
 import React from 'react'
-import { Input } from '@/ui/input'
-import { Label } from '@/ui/label'
+import { Input } from '@components/ui/input'
+import { Label } from '@components/ui/label'
+import { Button } from '@components/ui/button'
 
 const Home = () => {
+	const handleSubmit = async (event: React.FormEvent) => {
+		event.preventDefault()
+
+		const result = await fetch('http://localhost:3003/api/v1/auth/login', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				user: {
+					email: 'nuno@email.com',
+					password: '12345',
+				},
+			}),
+		})
+			.then((data) => data.json())
+			.then((data) => console.log(data))
+	}
+
 	return (
 		<div className="p-8 flex gap-12 flex-col items-center mx-auto max-w-96">
 			<h1 className="text-5xl">Login</h1>
 
-			<form method="POST" onSubmit={(e) => e.preventDefault()} className="w-full" action="">
+			<form onSubmit={handleSubmit} className="w-full">
 				<div>
 					<Label htmlFor="email">Email</Label>
 					<Input id="email" />
@@ -16,6 +37,9 @@ const Home = () => {
 				<div className="mt-6">
 					<Label htmlFor="password">Password</Label>
 					<Input id="password" />
+				</div>
+				<div className="mt-6">
+					<Button type="submit">Submit</Button>
 				</div>
 			</form>
 		</div>
