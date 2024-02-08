84e766566015d4bf136f2eec8a13fe72a5f7b690
Nuno <nunojllemos@gmail.com>
2024-02-08 14:41:54 +0100

    fix: bad import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Button } from 'antd'
 
 const HomePage = () => (
 	<div className="App">
 		<div>
 			<h1>Company Manager</h1>
 		</div>
-		<Button type="primary">Button</Button>
 	</div>
 )
 
