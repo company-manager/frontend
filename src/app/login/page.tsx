d483d8da56e2982d324e37ffe6f222eb683d88d6
Nuno <nunojllemos@gmail.com>
2024-02-08 17:36:38 +0100

    fix: delete package-lock.json

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,23 @@
+'use client'
 import React from 'react'
-import { Input } from '@/components/ui/input'
+import { Input } from '@/ui/input'
+import { Label } from '@/ui/label'
 
 const Home = () => {
 	return (
-		<div className="p-8 flex flex-col gap-8 max-w-96 mx-auto items-center">
+		<div className="p-8 flex gap-12 flex-col items-center mx-auto max-w-96">
 			<h1 className="text-5xl">Login</h1>
-			<div className="mt-8 w-full">
-				<form method="POST" action="https://companymanager.space/api/v1/auth/login">
-					<Input className="w-full" />
-				</form>
-			</div>
+
+			<form method="POST" onSubmit={(e) => e.preventDefault()} className="w-full" action="">
+				<div>
+					<Label htmlFor="email">Email</Label>
+					<Input id="email" />
+				</div>
+				<div className="mt-6">
+					<Label htmlFor="password">Password</Label>
+					<Input id="password" />
+				</div>
+			</form>
 		</div>
 	)
 }
