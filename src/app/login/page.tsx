efda62e3dcdabe0f80333bfb15b3a765e40e2eae
Nuno <nunojllemos@gmail.com>
2024-02-20 08:44:53 +0000

    dev: login form first commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,27 +4,36 @@ import { Input } from '@ui/input'
 import { Label } from '@ui/label'
 import { Button } from '@ui/button'
 import useLogin from '@hooks/useLogin'
+import { useForm } from 'react-hook-form'
 
 const Home = () => {
 	const { login, error } = useLogin()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
 
-	const handleSubmit = async (event: React.FormEvent) => {
-		event.preventDefault()
-
-		await login({
-			email: 'nuno@email.com',
-			password: '12345',
-		})
+	const onSubmit = async (data) => {
+		const { email, password } = data
+		await login({ email, password })
 	}
 
 	return (
 		<div className="p-8 flex gap-12 flex-col items-center mx-auto max-w-96">
 			<h1 className="text-5xl">Login</h1>
 
-			<form onSubmit={handleSubmit} className="w-full">
+			<form onSubmit={handleSubmit(onSubmit)} className="w-full">
 				<div>
 					<Label htmlFor="email">Email</Label>
-					<Input id="email" />
+					<Input
+						id="email"
+						{...(register('email'),
+						{
+							required: true,
+							pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+						})}
+					/>
 					{error?.tip?.includes('Email') && (
 						<span className="text-xs text-red-400">
 							Email not valid
@@ -33,7 +42,11 @@ const Home = () => {
 				</div>
 				<div className="mt-6">
 					<Label htmlFor="password">Password</Label>
-					<Input id="password" />
+					<Input
+						type="password"
+						id="password"
+						{...register('password')}
+					/>
 					{error?.tip?.includes('Password') && (
 						<span className="text-xs text-red-400">
 							Password incorrect
